17a7e890d76a4f519cef7d5669ac387a13fe34bd
Marckel9804 <ckdgh10421@gmail.com>
2024-02-22 18:11:53 +0900

    First Push

--- src/main/java/org/example/Study/S0222/Multiplication.java
+++ src/main/java/org/example/Study/S0222/Multiplication.java
@@ -1,7 +1,10 @@
 package org.example.Study.S0222;
 
+import java.util.Scanner;
+
 public class Multiplication {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
     }
 }
