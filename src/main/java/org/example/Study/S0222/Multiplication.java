4ed845b260eb7c3896239cd93d0ae0599bc4dcf1
Marckel9804 <ckdgh10421@gmail.com>
2024-02-22 18:26:30 +0900

    Second Push

--- src/main/java/org/example/Study/S0222/Multiplication.java
+++ src/main/java/org/example/Study/S0222/Multiplication.java
@@ -5,6 +5,6 @@ import java.util.Scanner;
 public class Multiplication {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
+        int n = sc.nextInt();
     }
 }
